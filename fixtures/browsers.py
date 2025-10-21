150d58fd9da5d5a31a9b57069d9de6d1035a3d80
Okovik1 <okovik1@gmail.com>
2025-10-21 23:08:58 +0200

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,12 +1,6 @@
 import pytest
 from playwright.sync_api import sync_playwright, Page, Playwright
 
-# @pytest.fixture
-# def chromium_page():
-#     with sync_playwright() as playwright:
-#         browser = playwright.chromium.launch(headless=False)
-#         yield browser.new_page()
-
 @pytest.fixture
 def chromium_page(playwright: Playwright)->Page:
     browser = playwright.chromium.launch(headless=False)
